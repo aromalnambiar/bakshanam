40f6d0e2c1eafd6e1aaac31fcc311701c9c8d02f
aromalnambiar <aromalndr@gmail.com>
2023-11-03 06:17:40 +0530

    add to cart without duplicate

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,30 @@ const defualtCartData = {
 
 const cartReducer = (state, action) => {
     if (action.type === "ADD") {
-        const updatedItems = state.items.concat(action.item)
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amout
-     
+
+        const existingItemsInedx = state.items.findIndex(item => item.id === action.item.id)
+
+        const existingItems = state.items[existingItemsInedx]
+
+        let updatedItem;
+        let updatedItems;
+
+        if (joinItems) {
+            const updatedItem = {
+                ...existingItems,
+                amout: existingItems.amout + action.item.amout
+            }
+
+            updatedItems = [...state.items]
+            updatedItems[existingItemsInedx] = updatedItem
+
+
+        } else {
+            updatedItems = state.items.concat(action.item)
+        }
+
+        
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
