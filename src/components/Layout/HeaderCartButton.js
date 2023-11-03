340442b086d9b97140c3a3da1207e343ff0e0493
aromalnambiar <aromalndr@gmail.com>
2023-11-03 07:29:03 +0530

    Bumb effect when add or remove item in cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
- import React, { useContext } from 'react'
+ import React, { useContext, useEffect, useState } from 'react'
 import style from './HeaderCartButton.module.css'
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/CartContext'
@@ -7,20 +7,46 @@ function HeaderCartButton(props) {
 
   const cartCtx = useContext(CartContext)
 
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  
+  const { items } = cartCtx;
+ 
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  
+  const btnBump = `${style.cart__button} ${
+    btnIsHighlighted ? style.bump : ""
+  }`;
+  
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+    const bumpTimer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(bumpTimer);
+    };
+  }, [items]);
+  
 
 
-  const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
 
-    return curNumber + item.amount
-  }, 0)
+  
 
   return (
     <>
-    <button className={style.cart__button} onClick={props.onClick} >
+    <button className={btnBump} onClick={props.onClick} >
         <CartIcon/>
         <span>Your Cart</span>
         <span className={style.badge} >
-            {numberOfItems} 
+            {numberOfCartItems} 
         </span>
     </button>
     </>
